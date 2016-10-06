ae9e107e2a51e0fc8c67956f29603dfba80cfeba
Tomas Pierantoni <tomas.pierantoni@globant.com>
2016-10-06 09:58:34 -0300

    sendind errors

---
+++ tdd-1/src/main/java/com/tom/bootcamp/App.java
@@ -0,0 +1,24 @@
+package com.tom.bootcamp;
+
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static List<String> listRecentFiles(){
+		List<String> files = new ArrayList<String>();
+		files.add("annoyingFile");
+		return files;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
