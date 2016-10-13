c7909cfb097438db957b14da1dfc5e4361cb7ff0
Tomas Pierantoni <tomas.pierantoni@globant.com>
2016-10-13 13:02:23 -0300

    documented first 2 methods

--- shoppingCartService/src/main/java/com/tom/bootcamp/CartService.java
+++ shoppingCartService/src/main/java/com/tom/bootcamp/CartService.java
@@ -2,10 +2,18 @@ package com.tom.bootcamp;
 
 import java.util.List;
 
+
 public interface CartService{
 	
 	//add/remove products from the cart
+	/* addToCart and deleteFromCart methods are the ones used to administrate the items on the list*/
+	
+	/* addToCart takes as argument a String, this string should be the name of the item
+	 * that the client wants to add to the cart, the order in the cart wil be the default
+	 * order given by the java.util.List class */
+	
 	public void addToCart(String item);
+	
 	
 	public void deleteFromCart(int index);
 	
