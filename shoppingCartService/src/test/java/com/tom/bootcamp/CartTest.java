79ad0fe30176a0204b15aa6d56565bfffd38f998
Tomas Pierantoni <tomas.pierantoni@globant.com>
2016-10-13 12:20:22 -0300

    removed unnecessary modules

--- shoppingCartService/src/test/java/com/tom/bootcamp/CartTest.java
+++ shoppingCartService/src/test/java/com/tom/bootcamp/CartTest.java
@@ -2,34 +2,11 @@ package com.tom.bootcamp;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CartTest {
 
 	private CartImplementation testImp = new CartImplementation();
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void canAdd() {
