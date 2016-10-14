ffc5c65a3c57d6405324973a5d603505e3bda800
Tomas Pierantoni <tomas.pierantoni@globant.com>
2016-10-14 09:22:47 -0300

    added javadoc comments

--- shoppingCartService/src/main/java/com/tom/bootcamp/CartService.java
+++ shoppingCartService/src/main/java/com/tom/bootcamp/CartService.java
@@ -5,22 +5,31 @@ import java.util.List;
 
 public interface CartService{
 	
-	//add/remove products from the cart
-	/* addToCart and deleteFromCart methods are the ones used to administrate the items on the list*/
+	/**<h5>Add/remove products from the cart</h5>
+	 addToCart and deleteFromCart methods are the ones used to administrate the items on the list
 	
-	/* addToCart takes as argument a String, this string should be the name of the item
+	 * @param item This string should be the name of the item
 	 * that the client wants to add to the cart, the order in the cart wil be the default
 	 * order given by the java.util.List class */
 	
 	public void addToCart(String item);
 	
-	
+	/**@param index This argument should be the index of the element the client wants to delete from the cart */
 	public void deleteFromCart(int index);
 	
-	
-	// get all the products in the cart
+	/** <h5>get all the elements from the cart</h5>
+	 * 
+	 * this method takes no parameters and returns an ArrayList containing the items from the cart 
+	 * ordered by order of addition
+	 */
 	public List<String> listCart();
 	
-	// clear products list and hypothetically make the buy
+	
+	/**<h5>Clear cart</h5>
+	 * 
+	 * This method calls .clear() on the cart items ArayList, we assume that this action 
+	 * is followed by the server logic to effectively realize the buy
+	 * */
+	
 	public void finishBuy();
 }
